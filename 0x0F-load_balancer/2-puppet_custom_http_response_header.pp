0a1db9f53c7420d8d9db830dd003e5027c40412f
MUONYELU CHINEDU UCHENNA <94756986+Nyelus101@users.noreply.github.com>
2023-06-05 19:24:20 +0100

    Delete 0x0F-load_balancer directory

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++
@@ -1,8 +0,0 @@
-# Using puppet for custom http response header
-
-file_line { 'nginx_custom_header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => "add_header X-Served-By $hostname;",
-  match => "^http {",
-}
-
